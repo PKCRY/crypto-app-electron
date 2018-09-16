498b33aa1141b2906d78a0409ca991035e95e778
Pablo Satler <pablosatler@gmail.com>
2018-09-15 21:57:49 -0300

    feat: create getBTC function to request the price of Bitcoin every 30s

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const electron = require('electron');
 const path = require('path'); //for working with files
 const BrowserWindow = electron.remote.BrowserWindow; //it's gonna allow us to create an actually window
+const axios = require('axios')
 
 const notifyBtn = document.getElementById('notifyBtn') //refering the Notify Me When button
+const price = document.querySelector('h1')
+const targetPrice = document.getElementById('targetPrice')
+
+
+function getBTC() {
+    // https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
+        .then( res => {
+            const cryptos = res.data.BTC.USD
+            price.innerHTML = `$ ${cryptos.toLocaleString('en')}` 
+        })
+}
+getBTC(); //calling the function to run it
+setInterval(getBTC, 30000); //then calling again every 30s
+
 
 notifyBtn.addEventListener('click', function(event) { //loads the add window
     const modalPath = path.join('file://', __dirname, 'add.html');
